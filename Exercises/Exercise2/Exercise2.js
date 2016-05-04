e28641143a1dfefd6459dd98317af101b6da9566
Jose Leon <jleon@tekmexico.com>
2016-05-04 14:58:15 -0700

    Started with Exercise 2

--- Exercises/Exercise2/Exercise2.js
+++ Exercises/Exercise2/Exercise2.js
@@ -1,3 +1,16 @@
 /**
  * Created by Jose Leon on 5/4/2016.
  */
+
+function draw() {
+    var canvas = document.querySelector('#canvas');
+    if (canvas.getContext) var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect(10, 10, 55, 50);
+
+    ctx.fillStyle="rgba(0,200,150,0.5)";
+    ctx.fillRect(30,30,55,50);
+}
+
+draw();
