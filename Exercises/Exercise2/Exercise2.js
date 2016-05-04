05e610c47f77f16598123a91b81ae8d5c685014e
Jose Leon <jleon@tekmexico.com>
2016-05-04 15:30:25 -0700

    Started with exercise 2

--- Exercises/Exercise2/Exercise2.js
+++ Exercises/Exercise2/Exercise2.js
@@ -3,14 +3,22 @@
  */
 
 function draw() {
+
+    function chooseFigure(){
+        var figure= document.getElementsByName('figure');
+        var figures = document.getElementsByTagName('input');
+        console.log(figure);
+        for(var i=0; i<figures.length;i++){
+            //if(figures[i].)
+            //console.log(figures[i]);
+        }
+    }
     var canvas = document.querySelector('#canvas');
     if (canvas.getContext) var ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = "rgb(200,0,0)";
+    chooseFigure();
+    ctx.fillStyle = "rgba(0,0,0,0.3)";
     ctx.fillRect(10, 10, 55, 50);
 
-    ctx.fillStyle="rgba(0,200,150,0.5)";
-    ctx.fillRect(30,30,55,50);
 }
-
 draw();
